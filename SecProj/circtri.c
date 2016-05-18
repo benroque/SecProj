dcacc3fa00c243270c101e70f98caceaf43f968e
benroque <benjamin.m.roque@lawrence.edu>
2016-05-17 19:19:31 -0500

    First commit

---
+++ SecProj/circtri.c
@@ -0,0 +1,15 @@
+
+# Please enter the commit message for your changes. Lines starting
+# with '#' will be ignored, and an empty message aborts the commit.
+# On branch master
+# Your branch is up-to-date with 'origin/master'.
+#
+# Changes to be committed:
+#	new file:   SecProj/circtri
+#	new file:   SecProj/circtri.c
+#	new file:   SecProj/circtri.o
+#	new file:   SecProj/makefile
+#	new file:   SecProj/triangle.c
+#	new file:   SecProj/triangle.h
+#	new file:   SecProj/triangle.o
+#
